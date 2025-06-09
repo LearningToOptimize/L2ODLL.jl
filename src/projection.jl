cbace99cb363f9311a0da3ab654f0e5b0a0a6295
Klamkin, Michael <mklamkin3@gatech.edu>
2025-06-09 14:27:58 -0400

    refactor projection_fn

--- src/projection.jl
+++ src/projection.jl
@@ -5,19 +5,21 @@ function make_proj_fn(decomposition::AbstractDecomposition, dual_model::JuMP.Mod
     ]
 
     # TODO: detect if there are any constraints in only y and p that we aren't projecting on to
-    return (y_prediction) -> [
-        if isnothing(set)
-            y
-        elseif length(y) == 1 && (
-            set isa MOI.EqualTo ||
-            set isa MOI.LessThan ||
-            set isa MOI.GreaterThan
-            # TODO: add other sets with one dual variable
-        )
-            [MOSD.projection_on_set(MOSD.DefaultDistance(), only(y), set)]
-        else
-            MOSD.projection_on_set(MOSD.DefaultDistance(), y, set)
-        end
-        for (set, y) in zip(sets, y_prediction)
-    ]
+    return (y_prediction) -> _projection_fn.(sets, y_prediction)
+end
+
+function _projection_fn(set, y)
+    if isnothing(set)
+        return y
+    elseif length(y) == 1 && (
+        set isa MOI.EqualTo ||
+        set isa MOI.LessThan ||
+        set isa MOI.GreaterThan
+        # TODO: add other sets with one dual variable
+    )
+        # vector type here?
+        return [MOSD.projection_on_set(MOSD.DefaultDistance(), only(y), set)]
+    else
+        return MOSD.projection_on_set(MOSD.DefaultDistance(), y, set)
+    end
 end
