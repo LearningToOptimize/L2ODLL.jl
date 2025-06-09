98bd721b1bb7d3e19b58a2e8b5dd206ccbdeb9e8
Documenter.jl <documenter@juliadocs.github.io>
2025-06-09 21:27:12 +0000

    build based on ca3204e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#L2ODLL.jl","page":"Home","title":"L2ODLL.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for L2ODLL.jl","category":"page"},{"location":"","page":"Home","title":"Home","text":"warning: Warning\nThis documentation is a work in progress. Please open an issue if content is missing / erroneous","category":"page"}]
+}
